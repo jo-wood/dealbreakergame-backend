723967aa411f8d8ed75f121f8d58648aecada4ab
Joanna Wood <caitlinjoannawood@gmail.com>
2019-07-24 14:57:39 -0400

    added route for /profile with routes/users currently returning ALL users, change to return only one user from req body cookie id

---
+++ server/routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+  router.get("/", (req, res) => {
+    console.log("profile GET route hit");
+
+    knex
+      .select()
+      .from('users')
+      .then((results) => {
+        console.log(results)
+        res.json(results);
+      });
+
+    });
+
+  return router
+}
