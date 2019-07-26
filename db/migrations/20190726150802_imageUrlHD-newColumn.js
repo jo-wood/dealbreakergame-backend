9fcbd29ecd4463aabb4b711e8ac1d475c46461f6
Alex Curci <alex.curci@gmail.com>
2019-07-26 15:30:36 -0400

    Update column add migration to include default image_hd string

--- db/migrations/20190726150802_imageUrlHD-newColumn.js
+++ db/migrations/20190726150802_imageUrlHD-newColumn.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.table('users', function(t) {
-    t.string('image_url_hd').defaultTo(0);
+    t.string('image_url_hd').defaultTo('https://upload.wikimedia.org/wikipedia/commons/f/f3/Nicolas_Cage_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29.jpg');
   });
 };
 
