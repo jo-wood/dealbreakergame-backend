38854d775260677e5067860dc6b2c4e3ce11fab5
Alex Curci <alex.curci@gmail.com>
2019-07-26 08:07:37 -0400

    profile routes added to inlcude :id, :id/matches, still required match details

--- server/routes/users.js
+++ server/routes/users.js
@@ -34,51 +34,28 @@ module.exports = (knex) => {
 
   // Return user info matching id
   router.get("/:id/matches", (req, res) => {
-  console.log("profile GET with ID and Match route hit");
-  const userId = req.params.id;
-    
-    knex
-      .select('*', knex.ref('user_b').as('x'))
-      .from('match')
-      .where('user_a', userId)
-      .union([
-        knex.select('*', knex.ref('user_a').as('x'))
-          .from('match')
-          .where('user_b', userId) 
-      ])
-      .innerJoin('users', 'x', 'users.id')
+    console.log("profile GET with ID and Match route hit");
+    const userId = req.params.id;
 
-
-
-
+    knex.raw( `select *
+      from  (select user_a x from match where user_b = ${userId} UNION select user_b x from match where user_a = ${userId}) as u 
+      inner join users on u.x = users.id;`)  
       .then((results) => {
         console.log(results)
-        res.json(results);
+        res.json(results.rows);
       });
     
       //   knex('users')
       //    .select(knex.ref('Id').as('UserId'))
-      // 
-      
-      // select `Id` as `UserId` from `users`
      
-5
+ });
+
+
+  router.get("/:id/matches/details", (req, res) => {
+    console.log("profile GET with ID and Match route hit");
+
+    
   });
-
-
-    // router.get("/:id/matches/:match_id", (req, res) => {
-    //   console.log("profile GET with ID and Match route hit");
-  
-    //   knex
-    //     .select()
-    //     .from('users')
-    //     .where('id', req.params.id)
-    //     .then((results) => {
-    //       console.log(results)
-    //       res.json(results);
-    //     });
-  
-    // });
 
 
 
