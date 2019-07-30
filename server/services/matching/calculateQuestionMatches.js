da6bd27cfe08fcef561b668789643299f25b3a6f
Junaid <jahmad095@gmail.com>
2019-07-30 18:15:05 -0400

    testing to see if we can get the perfect matches

--- server/services/matching/calculateQuestionMatches.js
+++ server/services/matching/calculateQuestionMatches.js
@@ -5,8 +5,24 @@ module.exports = {
   calculateQuestionMatches: (questionAnswers, questionIndex, callback) => {
     currentAnswerData = questionAnswers[questionIndex];
     let results = {};
+    let perfectMatches = {};
     for (currentUser in currentAnswerData) {
-      results[currentUser] = calculatePerQuestionMatch(currentAnswerData[currentUser], currentAnswerData);   
+      results[currentUser] = calculatePerQuestionMatch(currentAnswerData[currentUser], currentAnswerData);
+      
+      for (matchUser in results[currentUser]) {
+        if (results[currentUser][matchUser] === 100) {
+          perfectMatches[currentUser] = { matchUser: {
+            q_id: questionIndex,
+            user_b_answered: currentAnswerData[currentUser],
+            sameAnswer: true
+          }}
+        }
+      }
+
+      //determine if the match precentage is 100%
+      if (results[currentUser] === 100) {
+        //log this question id and pass back to server.js ONLY if its top 3
+      }
     }
 
     // let newResults = calculateMatchAverage([results], 2, (averageData) => {
@@ -14,6 +30,7 @@ module.exports = {
 
     // });
 
+    console.log("PERFECT MATCHES",perfectMatches)
     callback(results); 
   }
 
